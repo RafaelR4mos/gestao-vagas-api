814ba9732b6d53b6af8ff8576a32b44bb1081bc6
RafaelR4mos <rafaelramos0401@gmail.com>
2024-05-20 21:16:02 -0300

    wip in creating candidates table

--- src/main/java/com/rafael/vagas_api/modules/candidate/CandidateEntity.java
+++ src/main/java/com/rafael/vagas_api/modules/candidate/CandidateEntity.java
@@ -1,18 +1,26 @@
 package com.rafael.vagas_api.modules.candidate;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import jakarta.validation.constraints.Email;
 import lombok.NonNull;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Entity(name = "Candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @Pattern(regexp = "^[^\\s]*$", message = "O campo [username] não deve conter espaços")
@@ -27,4 +35,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
